f51f976791bb3c6c7dfdc6ed87037daa2598274c
roman.lebedenko <roman.lebedenko@engelvoelkers.com>
2024-12-26 16:33:34 +0100

    added swagger.json

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import { Response } from 'express'; // Import Express Response
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -18,7 +20,14 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  const swaggerJsonPath = './swagger.json';
+  writeFileSync(swaggerJsonPath, JSON.stringify(document, null, 2));
+  SwaggerModule.setup('/api/docs', app, document);
+
+  // Serve swagger.json
+  app.getHttpAdapter().get('/api/docs/swagger.json', (req, res: Response) => {
+    res.sendFile(swaggerJsonPath, { root: process.cwd() });
+  });
 
   await app.listen(port);
 }
