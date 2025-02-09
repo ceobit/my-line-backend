e4b14be8dc649dbef3a7b7dee9e30782b1dc65df
roman.lebedenko <ceobit90@gmail.com>
2025-02-09 18:40:15 +0100

    removed synchronised true

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,22 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => ({
-                type: 'postgres',
-                host: configService.get('DB_HOST'),
-                port: configService.get<number>('DB_PORT'),
-                username: configService.get('DB_USERNAME'),
-                password: configService.get('DB_PASSWORD'),
-                database: configService.get('DB_NAME'),
-                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-                synchronize: true, // Don't use this in production
-            }),
-        }),
-    ]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      }),
+    }),
+  ],
 })
-
 export class DatabaseModule {}
